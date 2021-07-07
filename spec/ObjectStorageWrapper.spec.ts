ba93b94126bf9ba4dda3e91aa4547c7cd3ba6ce1
Ilya <soroka14021998@gmail.com>
2021-07-07 21:11:07 +0300

    Fixes (#14)
    Remove extra validation from lookupObjectById and updateObject methods

--- spec/ObjectStorageWrapper.spec.ts
+++ spec/ObjectStorageWrapper.spec.ts
@@ -8,7 +8,8 @@ import { ObjectStorageWrapper, MAESTER_MAX_SUPPORTED_COUNT_OF_QUERY_HEADERS } fr
 
 const { expect } = chai;
 
-process.env.ELASTICIO_OBJECT_STORAGE_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRJZCI6IjU2YzIwN2FkYjkxMjExODFlNjUwYzBlZiIsImNvbnRyYWN0SWQiOiI1YjVlZDFjZjI3MmNmODAwMTFhZTdiNmEiLCJ3b3Jrc3BhY2VJZCI6IjVhNzFiZmM1NjA3ZjFiMDAwNzI5OGEyYSIsImZsb3dJZCI6IioiLCJ1c2VySWQiOiI1YjE2NGRiMzRkNTlhODAwMDdiZDQ3OTMiLCJpYXQiOjE1ODg1ODg3NjZ9.3GlJAwHz__e2Y5tgkzD1t-JyhgXGJOSVFSLUBCqLh5Y';
+process.env.ELASTICIO_OBJECT_STORAGE_TOKEN =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRJZCI6IjU2YzIwN2FkYjkxMjExODFlNjUwYzBlZiIsImNvbnRyYWN0SWQiOiI1YjVlZDFjZjI3MmNmODAwMTFhZTdiNmEiLCJ3b3Jrc3BhY2VJZCI6IjVhNzFiZmM1NjA3ZjFiMDAwNzI5OGEyYSIsImZsb3dJZCI6IioiLCJ1c2VySWQiOiI1YjE2NGRiMzRkNTlhODAwMDdiZDQ3OTMiLCJpYXQiOjE1ODg1ODg3NjZ9.3GlJAwHz__e2Y5tgkzD1t-JyhgXGJOSVFSLUBCqLh5Y';
 process.env.ELASTICIO_WORKSPACE_ID = 'test';
 process.env.ELASTICIO_FLOW_ID = 'test';
 process.env.ELASTICIO_API_URI = 'https://api.hostname';
@@ -26,8 +27,6 @@ describe('ObjectStorageWrapper', () => {
     }
     return resultHeaders;
   };
-  const objectNotFoundResponse = 'Object Not Found';
-  const invalidIdResponse = 'Invalid object id';
   const data = {
     foo: 'bar',
   };
@@ -104,16 +103,11 @@ describe('ObjectStorageWrapper', () => {
       });
       describe('Without queriable fields', () => {
         it('Should save the data correctly', async () => {
-          nock(maesterUri)
-            .post('/objects')
-            .matchHeader('x-eio-ttl', '-1')
-            .reply(201, createObjectWithQueriableField);
+          nock(maesterUri).post('/objects').matchHeader('x-eio-ttl', '-1').reply(201, createObjectWithQueriableField);
           await objectStorageWrapper.createObject(data, [], ttl);
         });
         it('Should save the data correctly', async () => {
-          nock(maesterUri)
-            .post('/objects')
-            .reply(201, createObjectWithQueriableField);
+          nock(maesterUri).post('/objects').reply(201, createObjectWithQueriableField);
           await objectStorageWrapper.createObject(data);
         });
       });
@@ -121,32 +115,41 @@ describe('ObjectStorageWrapper', () => {
     describe('invalid inputs', () => {
       describe('Query key set, query value undefined', () => {
         it('Should throw error', async () => {
-          await objectStorageWrapper.createObject(data, [{ key: 'key0', value: 'value0' }, { key: 'key1' }], ttl)
-            .catch((error: { message: any; }) => {
+          await objectStorageWrapper
+            .createObject(data, [{ key: 'key0', value: 'value0' }, { key: 'key1' }], ttl)
+            .catch((error: { message: any }) => {
               expect(error.message).to.equal('header "value" is mandatory if header "key" passed');
             });
         });
       });
       describe('Query value set, query key undefined', () => {
         it('Should throw error', async () => {
-          await objectStorageWrapper.createObject(data, [{ value: 'value1' }], ttl)
-            .catch((error: { message: any; }) => {
-              expect(error.message).to.equal('header "key" is mandatory if header "value" passed');
-            });
+          await objectStorageWrapper.createObject(data, [{ value: 'value1' }], ttl).catch((error: { message: any }) => {
+            expect(error.message).to.equal('header "key" is mandatory if header "value" passed');
+          });
         });
       });
       describe(`Maester headers maximum amount is exceed (${MAESTER_MAX_SUPPORTED_COUNT_OF_QUERY_HEADERS} items)`, () => {
         it('Should throw error', async () => {
-          await objectStorageWrapper.createObject(data, genHeaders(MAESTER_MAX_SUPPORTED_COUNT_OF_QUERY_HEADERS + 1), ttl)
-            .catch((error: { message: any; }) => {
+          await objectStorageWrapper
+            .createObject(data, genHeaders(MAESTER_MAX_SUPPORTED_COUNT_OF_QUERY_HEADERS + 1), ttl)
+            .catch((error: { message: any }) => {
               expect(error.message).to.equal(`maximum available amount of headers is ${MAESTER_MAX_SUPPORTED_COUNT_OF_QUERY_HEADERS}`);
             });
         });
       });
       describe('Header used more than one time', () => {
         it('Should throw error', async () => {
-          await objectStorageWrapper.createObject(data, [{ key: 'key0', value: 'value0' }, { key: 'key0', value: 'value0' }], ttl)
-            .catch((error: { message: any; }) => {
+          await objectStorageWrapper
+            .createObject(
+              data,
+              [
+                { key: 'key0', value: 'value0' },
+                { key: 'key0', value: 'value0' },
+              ],
+              ttl,
+            )
+            .catch((error: { message: any }) => {
               expect(error.message).to.equal('header key "key0" was already added');
             });
         });
@@ -154,38 +157,14 @@ describe('ObjectStorageWrapper', () => {
     });
   });
   describe('Lookup object by ID', () => {
-    describe('Object not found in Maester', () => {
-      it('Should throw "Object not found"', async () => {
-        nock(maesterUri)
-          .get(`/objects/${id}`)
-          .reply(200, objectNotFoundResponse);
-        await objectStorageWrapper.lookupObjectById(id).catch((error: { message: any; }) => {
-          expect(error.message).to.equal('Object Not Found');
-        });
-      });
-    });
-    describe('ID is invalid', () => {
-      it('Should throw "Invalid object id"', async () => {
-        nock(maesterUri)
-          .get(`/objects/${id}`)
-          .reply(200, invalidIdResponse);
-        await objectStorageWrapper.lookupObjectById(id).catch((error: { message: any; }) => {
-          expect(error.message).to.equal('Invalid object id');
-        });
-      });
-    });
     describe('Lookup with valid ID', () => {
       it('Should successfully return a JSON object', async () => {
-        nock(maesterUri)
-          .get(`/objects/${id}`)
-          .reply(200, data);
+        nock(maesterUri).get(`/objects/${id}`).reply(200, data);
         const result = await objectStorageWrapper.lookupObjectById(id);
         expect(result).to.deep.equal(rawData);
       });
       it('Should successfully return a string', async () => {
-        nock(maesterUri)
-          .get(`/objects/${id}`)
-          .reply(200, rawData);
+        nock(maesterUri).get(`/objects/${id}`).reply(200, rawData);
         const result = await objectStorageWrapper.lookupObjectById(id);
         expect(result).to.deep.equal(rawData);
       });
@@ -194,9 +173,7 @@ describe('ObjectStorageWrapper', () => {
   describe('Lookup object by query parameter', () => {
     describe('Object not found in Maester', () => {
       it('Should successfully return an empty stringified array', async () => {
-        nock(maesterUri)
-          .get(`/objects?query[${queryKey}]=${queryValue}`)
-          .reply(200, []);
+        nock(maesterUri).get(`/objects?query[${queryKey}]=${queryValue}`).reply(200, []);
         const result = await objectStorageWrapper.lookupObjectsByQueryParameters([{ key: queryKey, value: queryValue }]);
         expect(result).to.deep.equal([]);
       });
@@ -204,16 +181,12 @@ describe('ObjectStorageWrapper', () => {
     describe('Different amount of search params', () => {
       describe('valid input', () => {
         it('Should successfully return an empty stringified array', async () => {
-          nock(maesterUri)
-            .get('/objects')
-            .reply(200, []);
+          nock(maesterUri).get('/objects').reply(200, []);
           const result = await objectStorageWrapper.lookupObjectsByQueryParameters([]);
           expect(result).to.deep.equal([]);
         });
         it('Should successfully return an empty stringified array', async () => {
-          nock(maesterUri)
-            .get('/objects?query[key0]=value0')
-            .reply(200, []);
+          nock(maesterUri).get('/objects?query[key0]=value0').reply(200, []);
           const result = await objectStorageWrapper.lookupObjectsByQueryParameters(genHeaders(1));
           expect(result).to.deep.equal([]);
         });
@@ -226,9 +199,7 @@ describe('ObjectStorageWrapper', () => {
         });
         describe('One object found in Maester', () => {
           it('Should return a stringified array of 1 object', async () => {
-            nock(maesterUri)
-              .get('/objects?query[key0]=value0&query[key1]=value1')
-              .reply(200, [createObjectWithQueriableField]);
+            nock(maesterUri).get('/objects?query[key0]=value0&query[key1]=value1').reply(200, [createObjectWithQueriableField]);
             const result = await objectStorageWrapper.lookupObjectsByQueryParameters(genHeaders(2));
             expect(result).to.deep.equal([createObjectWithQueriableField]);
           });
@@ -246,32 +217,40 @@ describe('ObjectStorageWrapper', () => {
       describe('invalid input', () => {
         describe('Query key set, query value undefined', () => {
           it('Should throw error', async () => {
-            await objectStorageWrapper.lookupObjectsByQueryParameters([{ key: 'key0', value: 'value0' }, { key: 'key1' }], ttl)
-              .catch((error: { message: any; }) => {
+            await objectStorageWrapper
+              .lookupObjectsByQueryParameters([{ key: 'key0', value: 'value0' }, { key: 'key1' }], ttl)
+              .catch((error: { message: any }) => {
                 expect(error.message).to.equal('header "value" is mandatory if header "key" passed');
               });
           });
         });
         describe('Query value set, query key undefined', () => {
           it('Should throw error', async () => {
-            await objectStorageWrapper.lookupObjectsByQueryParameters([{ value: 'value1' }], ttl)
-              .catch((error: { message: any; }) => {
-                expect(error.message).to.equal('header "key" is mandatory if header "value" passed');
-              });
+            await objectStorageWrapper.lookupObjectsByQueryParameters([{ value: 'value1' }], ttl).catch((error: { message: any }) => {
+              expect(error.message).to.equal('header "key" is mandatory if header "value" passed');
+            });
           });
         });
         describe(`Maester headers maximum amount is exceed (${MAESTER_MAX_SUPPORTED_COUNT_OF_QUERY_HEADERS} items)`, () => {
           it('Should throw error', async () => {
-            await objectStorageWrapper.lookupObjectsByQueryParameters(genHeaders(MAESTER_MAX_SUPPORTED_COUNT_OF_QUERY_HEADERS + 1), ttl)
-              .catch((error: { message: any; }) => {
+            await objectStorageWrapper
+              .lookupObjectsByQueryParameters(genHeaders(MAESTER_MAX_SUPPORTED_COUNT_OF_QUERY_HEADERS + 1), ttl)
+              .catch((error: { message: any }) => {
                 expect(error.message).to.equal(`maximum available amount of headers is ${MAESTER_MAX_SUPPORTED_COUNT_OF_QUERY_HEADERS}`);
               });
           });
         });
         describe('Header used more than one time', () => {
           it('Should throw error', async () => {
-            await objectStorageWrapper.lookupObjectsByQueryParameters([{ key: 'key0', value: 'value0' }, { key: 'key0', value: 'value0' }], ttl)
-              .catch((error: { message: any; }) => {
+            await objectStorageWrapper
+              .lookupObjectsByQueryParameters(
+                [
+                  { key: 'key0', value: 'value0' },
+                  { key: 'key0', value: 'value0' },
+                ],
+                ttl,
+              )
+              .catch((error: { message: any }) => {
                 expect(error.message).to.equal('header key "key0" was already added');
               });
           });
@@ -280,62 +259,17 @@ describe('ObjectStorageWrapper', () => {
     });
   });
   describe('Update object', () => {
-    describe('Object not found in Maester', () => {
-      it('Should return "object not found"', async () => {
-        nock(maesterUri)
-          .get(`/objects/${id}`)
-          .reply(200, objectNotFoundResponse);
-        await objectStorageWrapper.updateObject(id, data).catch((error: { message: any; }) => {
-          expect(error.message).to.equal('No objects found with id id123');
-        });
-      });
-    });
-    describe('ID is invalid', () => {
-      it('Should return "invalid object id"', async () => {
-        nock(maesterUri)
-          .get(`/objects/${id}`)
-          .reply(200, invalidIdResponse);
-        await objectStorageWrapper.updateObject(id, data).catch((error: { message: any; }) => {
-          expect(error.message).to.equal('Invalid object id id123');
-        });
-      });
-    });
     describe('Valid update request', () => {
       it('Should successfully update an object', async () => {
-        nock(maesterUri)
-          .get(`/objects/${id}`)
-          .reply(200, data);
-        nock(maesterUri)
-          .put(`/objects/${id}`)
-          .reply(200, updatedData);
+        nock(maesterUri).put(`/objects/${id}`).reply(200, updatedData);
         const result = await objectStorageWrapper.updateObject(id, updatedData);
         expect(result).to.deep.equal(updatedData);
       });
     });
     describe('Delete object', () => {
-      describe('Object not found in Maester', () => {
-        it('Should return "object not found"', async () => {
-          nock(maesterUri)
-            .delete(`/objects/${id}`)
-            .reply(200, objectNotFoundResponse);
-          const result = await objectStorageWrapper.deleteObjectById(id);
-          expect(result).to.equal(objectNotFoundResponse);
-        });
-      });
-      describe('ID is invalid', () => {
-        it('Should return "invalid object id"', async () => {
-          nock(maesterUri)
-            .delete(`/objects/${id}`)
-            .reply(200, invalidIdResponse);
-          const result = await objectStorageWrapper.deleteObjectById(id);
-          expect(result).to.equal(invalidIdResponse);
-        });
-      });
       describe('ID is valid', () => {
         it('Should delete an object', async () => {
-          nock(maesterUri)
-            .delete(`/objects/${id}`)
-            .reply(204);
+          nock(maesterUri).delete(`/objects/${id}`).reply(204);
           const result = await objectStorageWrapper.deleteObjectById(id);
           expect(result).to.equal('');
         });
