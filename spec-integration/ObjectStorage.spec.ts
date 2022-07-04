a45bd05d7b3105f3ad1de86bd6758803657b2fd4
EmptyInfinity <soroka14021998@gmail.com>
2022-07-04 11:58:03 +0300

    update

--- spec-integration/ObjectStorage.spec.ts
+++ spec-integration/ObjectStorage.spec.ts
@@ -7,7 +7,7 @@ import { streamFromData } from '../src/utils';
 
 chai.use(require('chai-as-promised'));
 
-describe('objectStorag e', () => {
+describe('objectStorage', () => {
   const objectStorage = new ObjectStorage(creds);
   describe('add', () => {
     describe('as stream', () => {
